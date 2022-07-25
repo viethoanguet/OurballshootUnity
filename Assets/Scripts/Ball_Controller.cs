1b4ac305c85ab6c231fec63ac515ea92473cc1af
viethoanguet <phanviethoang187@gmail.com>
2022-07-25 11:31:07 +0700

    commit 1

---
+++ Assets/Scripts/Ball_Controller.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball_Controller : MonoBehaviour
+{
+    
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(Vector3.up * 300f);
+    
+    }
+}
